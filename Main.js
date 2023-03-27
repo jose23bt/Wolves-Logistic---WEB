73e81a3fe8a23a5090302fa3fb9dbd4123c4197e
Jose Becerra <jose.antonio23b@gmail.com>
2023-03-27 00:57:35 -0300

    arreglos al cotizador de zonas

--- Main.js
+++ Main.js
@@ -26,7 +26,7 @@ function calcRoute() {
     const request = {
         origin: document.getElementById("from").value,
         destination: document.getElementById("to").value,
-        travelMode: google.maps.TravelMode.DRIVING, 
+        travelMode: google.maps.TravelMode.DRIVING,
         unitSystem: google.maps.UnitSystem.METRIC,
     };
 
@@ -68,6 +68,7 @@ function calculateFare_km(kilometros) {
     return fare;
 }
 
+
 //COTIZADOR POR ZONAS
 
 // Obtener los botones
@@ -79,30 +80,32 @@ let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
 // Actualizar el carrito en el almacenamiento local
 function actualizarCarrito() {
-    // Obtener el elemento <ul> del carrito
     const carritoUL = document.querySelector('.carrito ul');
-
-    // Reiniciar el contenido del carrito
     carritoUL.innerHTML = '';
+    let total = 0;
+    const productosAgregados = {};
 
-    // Comprobar que el carrito tenga algún producto
-    if (carrito.length === 0) {
-        document.querySelector('.total').innerHTML = 'Total: $0.00';
-        return;
-    }
-
-    // Agregar cada producto al carrito
     carrito.forEach(producto => {
-        const li = document.createElement('li');
-        li.innerHTML = `${producto.nombre} <span>$${producto.precio}</span>`;
-        carritoUL.appendChild(li);
+        const nombreProducto = producto.nombre;
+        const precioProducto = parseFloat(producto.precio);
+        total += precioProducto;
+
+        // Agregar el producto al objeto productosAgregados y contar cuántas veces se ha agregado
+        if (nombreProducto in productosAgregados) {
+            productosAgregados[nombreProducto]++;
+        } else {
+            productosAgregados[nombreProducto] = 1;
+        }
     });
 
-    // Calcular el total y mostrarlo
-    const total = carrito.reduce((acumulador, producto) => acumulador + parseFloat(producto.precio), 0);
-    document.querySelector('.total').innerHTML = `Total: $${total.toFixed(2)}`;
+    for (const nombreProducto in productosAgregados) {
+        const cantidad = productosAgregados[nombreProducto];
+        const li = document.createElement('li');
+        li.innerHTML = `${nombreProducto} x${cantidad} <span>$${(cantidad * parseFloat(carrito.find(item => item.nombre === nombreProducto).precio)).toFixed(2)}</span>`;
+        carritoUL.appendChild(li);
+    }
 
-    // Actualizar el almacenamiento local
+    document.querySelector('.total').innerHTML = `Total: $${total.toFixed(2)}`;
     localStorage.setItem('carrito', JSON.stringify(carrito));
 }
 
@@ -137,3 +140,48 @@ botonesEliminar.forEach(boton => boton.addEventListener('click', eliminarProduct
 // Mostrar el carrito al cargar la página
 actualizarCarrito();
 
+function solicitarViaje() {
+    // Obtener los productos agrupados por nombre y sumar las cantidades
+    const productosAgrupados = carrito.reduce((acumulador, producto) => {
+        if (!acumulador[producto.nombre]) {
+            acumulador[producto.nombre] = {
+                nombre: producto.nombre,
+                precio: parseFloat(producto.precio),
+                cantidad: 1
+            };
+        } else {
+            acumulador[producto.nombre].precio += parseFloat(producto.precio);
+            acumulador[producto.nombre].cantidad++;
+        }
+        return acumulador;
+    }, {});
+
+    // Crear una cadena de consulta URL-encoded con los productos y sus cantidades
+    const queryString = Object.values(productosAgrupados)
+        .map(producto => `${encodeURIComponent(producto.nombre)} x${producto.cantidad}=${encodeURIComponent(producto.precio)}`)
+        .join('%0A');
+
+    // Obtener el precio total
+    const total = Object.values(productosAgrupados)
+        .reduce((acumulador, producto) => acumulador + producto.precio, 0);
+
+    // Crear el mensaje con los productos y el precio total
+    const mensaje = `¡Hola! Quiero solicitar un viaje con los siguientes productos:%0A%0A${queryString}%0A%0ATotal: $${total.toFixed(2)}`;
+
+    // Crear la URL de la conversación de WhatsApp con el mensaje
+    const url = `https://api.whatsapp.com/send/?phone=5491123318355&text=${mensaje}`;
+
+    // Abrir la conversación de WhatsApp en una pestaña nueva
+    window.open(url, '_blank');
+}
+
+ //REINICIAR COTIZADOR
+
+ const botonReiniciar = document.querySelector('.reiniciar');
+
+function reiniciarCotizador() {
+    carrito = [];
+    actualizarCarrito();
+}
+
+botonReiniciar.addEventListener('click', reiniciarCotizador);
